51fc7b1d0b3a94829890a516874f5f76870d997e
seong3056 <aktnf207@naver.com>
2024-06-03 14:24:39 +0900

    메인페이지 작성중

--- sign_translation/urls.py
+++ sign_translation/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('index.urls')),
     path('recognition/', include('recognition.urls')),  # recognition 앱의 urls.py 포함
 ]
