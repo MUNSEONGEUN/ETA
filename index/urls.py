c2d6f11d103009f36031ed2d0b725b88db30c014
seong3056 <aktnf207@naver.com>
2024-06-04 09:16:47 +0900

    유튜브 영상 페이지 구현중

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from index import views
 
 urlpatterns = [
-    path('', views.index, name='index'),    
+    path('', views.index, name='index'),
+    path('learning/', views.learning)
 ]
