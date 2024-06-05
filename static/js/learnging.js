ef5462d752f9c60ae37aa3a0e00cae5357bc18c5
seong3056 <aktnf207@naver.com>
2024-06-05 12:32:02 +0900

    네비게이션버튼 생성중

--- static/js/learnging.js
+++ static/js/learnging.js
@@ -98,3 +98,27 @@ group.addEventListener("click",(e)=>{
     video.innerHTML = json[title][select]
 
 })
+const sticky = document.querySelector(".sticky")
+window.onload = ()=>{
+    let url = window.location.href
+    url = url.substring(url.indexOf(":8000/")+6).split("/")    
+    url = url.slice(url.length-2, url.length-1)[0]
+    const e = sticky.querySelector("#"+url)
+    console.log(url);
+    sticky.removeChild(e)
+}
+
+sticky.addEventListener("mouseover",()=>{
+    // sticky.style.height = "150px";
+    
+    [...sticky.querySelectorAll(".link")].map(e=>{
+        e.classList.remove("hidden")
+    })
+})
+
+sticky.addEventListener("mouseout",()=>{
+    [...sticky.querySelectorAll(".link")].map(e=>{
+        // sticky.style.height = "50px"
+        e.classList.add("hidden")
+    })
+})
